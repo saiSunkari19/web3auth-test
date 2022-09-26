815dd8023e640a910e38d41f6f9f412e5f96d6b3
saisunkari19 <saisunkari19@protonmail.com>
2022-09-26 19:27:17 +0530

    feat: cosmosation wallet

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import { useEffect, useState} from 'react'
 import {Web3Auth} from '@web3auth/web3auth';
 import { WALLET_ADAPTERS, CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
 import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
-
+import { cosmos, InstallError } from "@cosmostation/extension-client";
+import { SEND_TRANSACTION_MODE } from "@cosmostation/extension-client/cosmos";
 
 
 const clientID ="BE8Aj_uSDOMsjzWj58KzVoDavC3yghwWIR4iFl7hv7IXgiGDBnBZypdveGq_OVP24Zt2S3n0iQkm5Y3y2ZV9SRg";
@@ -58,6 +59,14 @@ function App() {
      init();
   },[])
 
+const networkInfo = {
+  name:"assetmantle",
+  denom:"umntl",
+  chainId: "mantle-1",
+  decimal: 6,
+}
+
+
   const login = async() =>{
     if(!web3auth){
       console.log("web3auth not initalized yet");
@@ -99,6 +108,77 @@ function App() {
 
     await web3auth.logout()
   }
+
+
+  const handleWalletExtension = async() =>{
+    console.log("handle cosmostaion wallet extension")
+
+    try{
+      const provider = await cosmos()
+      
+
+      provider.onAccountChanged(() => console.log("changed"));
+
+      const chains = await provider.getSupportedChains()
+      console.log(chains)
+
+    
+
+
+      // Request Account 
+      const account = await provider.requestAccount(networkInfo.chainId);
+      console.log(account)
+    
+      const activatedChains = await provider.getSupportedChainIds();
+      console.log(activatedChains)
+
+      // Sign Message
+      const response = await provider.signAmino(
+        networkInfo.name,
+        {
+          chain_id: networkInfo.chainId,
+          fee: { amount: [{ denom: networkInfo.denom, amount: "5000" }], gas: "200000" },
+          memo: "test account",
+          msgs: [
+            {
+              type: "cosmos-sdk/MsgSend",
+              value: {
+                from_address: account.address,
+                to_address: account.address,
+                amount: [{ denom: networkInfo.denom, amount: "5000" }],
+              },
+            },
+          ],
+          sequence: "1",
+          account_number: "12",
+        },
+        { memo: true, fee: true } // edit | optional (default: { memo: false, fee: false }),
+      );
+      
+      console.log(response)
+
+      
+
+      const txResponse = await provider.sendTransaction(
+        networkInfo.name,
+        response.toString(), // base64 string or Uint8Array
+        SEND_TRANSACTION_MODE.BLOCK /* SEND_TRANSACTION_MODE or one of [0, 1, 2, 3] */
+      );
+
+      console.log(txResponse)
+
+
+    }catch(e:any){
+      if (e instanceof InstallError){
+        console.log("not Installed")
+      }
+      if (e.code === 4001){
+        console.log("user rejected request");
+      }
+      console.log(e)
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -130,6 +210,12 @@ function App() {
       <button onClick={logout} className="card">
         Log Out
       </button>
+
+      <button onClick={handleWalletExtension} className="card">
+        Connect CosmostationWallet
+      </button>
+
+
       </header>
     </div>
   );
